9bbc085367e0184cbdc1e43df609c7ad6d89e84f
fiissh.zhao <fiissh.zhao@gmail.com>
2018-10-23 15:53:06 +0800

    修正打包错误

--- app/src/main/java/com/qihoo360/replugin/sample/host/SimpleFragment.java
+++ app/src/main/java/com/qihoo360/replugin/sample/host/SimpleFragment.java
@@ -1,6 +1,5 @@
 package com.qihoo360.replugin.sample.host;
 
-import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,15 +8,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.qihoo360.replugin.RePlugin;
 
 public class SimpleFragment extends Fragment implements View.OnClickListener {
+
+    private static final int REQUEST_CODE_SIMPLE_A = 0x011;
+    private static final int RESULT_CODE_SIMPLE_A = 0x012;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.framgment_simple, null);
         rootView.findViewById(R.id.start_simple_a_by_package).setOnClickListener(this);
+        rootView.findViewById(R.id.start_simple_a_by_alias).setOnClickListener(this);
         return rootView;
     }
 
@@ -26,12 +31,33 @@ public class SimpleFragment extends Fragment implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.start_simple_a_by_package:
                 // 使用包名打开 Simple A 的 MainActivity
-                RePlugin.startActivity(getContext(), RePlugin.createIntent("com.qihoo360.replugin.sample.a", "com.qihoo360.replugin.sample.a.MainActivity"));
+                if (RePlugin.isPluginInstalled("com.qihoo360.replugin.sample.a")) {
+                    RePlugin.startActivity(getContext(), RePlugin.createIntent("com.qihoo360.replugin.sample.a", "com.qihoo360.replugin.sample.a.MainActivity"));
+                } else {
+                    Toast.makeText(getContext(), "插件 Simple A 没有安装", Toast.LENGTH_LONG).show();
+                }
 
-//                Intent intent = new Intent();
-//                intent.setComponent(new ComponentName("demo1", "com.qihoo360.replugin.sample.demo1.activity.for_result.ForResultActivity"));
-//                RePlugin.startActivityForResult(MainActivity.this, intent, REQUEST_CODE_DEMO1, null);
                 break;
+
+            case R.id.start_simple_a_by_alias:
+                // 使用别名打开 Simple B 的 MainActivity
+                if (RePlugin.isPluginInstalled("simple-b")) {
+                    RePlugin.startActivity(getContext(), RePlugin.createIntent("simple-b", "com.qihoo360.replugin.sample.b.MainActivity"));
+                } else {
+                    Toast.makeText(getContext(), "插件 Simple B 没有安装", Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SIMPLE_A && resultCode == RESULT_CODE_SIMPLE_A) {
+            String result = data.getExtras().getString("data");
+            Toast.makeText(getContext(), "来自于 Simple B 的 result " + result, Toast.LENGTH_LONG).show();
         }
     }
 }
--- replugin-plugin-sample-b/src/main/java/com/qihoo360/replugin/sample/b/MainActivity.java
+++ replugin-plugin-sample-b/src/main/java/com/qihoo360/replugin/sample/b/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Intent intent = new Intent();
+//        intent.putExtra("data", "来自 Simple B 的结果");
+//        setResult(0x022, intent);
+//        finish();
     }
+
 }
